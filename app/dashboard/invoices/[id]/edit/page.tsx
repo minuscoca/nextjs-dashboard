66a6056f422181e719c0dec9b94f5d5b49234eea
Kevin <minuscoca@gmail.com>
2023-11-14 21:53:38 +0800

    handle error

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -1,6 +1,7 @@
 import Form from '@/app/ui/invoices/edit-form';
 import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
 import { fetchInvoiceById, fetchCustomers } from '@/app/lib/data';
+import { notFound } from 'next/navigation';
 
 export default async function Page({
   params
@@ -15,7 +16,9 @@ export default async function Page({
     fetchCustomers(),
   ])
 
-  if (!invoice) return null
+  if (!invoice) {
+    notFound();
+  }
 
   return (
     <main>
